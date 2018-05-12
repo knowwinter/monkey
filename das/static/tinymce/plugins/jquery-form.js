692a1f0c1c6db34435812faa9267892a80722c5a
余晓冬 <yuxiaodong@xianglin.cn>
2018-05-13 02:19:27 +0800

    1.修复了评论ajax异步提交无法获取tinymce文本框内容的bug，在提交前需要先用tinymce保存

--- das/static/tinymce/plugins/jquery-form.js
+++ das/static/tinymce/plugins/jquery-form.js
@@ -134,6 +134,7 @@ $.fn.ajaxSubmit = function(options) {
         return this;
     }
 
+
     // provide opportunity to alter form data before it is serialized
     if (options.beforeSerialize && options.beforeSerialize(this, options) === false) {
         log('ajaxSubmit: submit aborted via beforeSerialize callback');
